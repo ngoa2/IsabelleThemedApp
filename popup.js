ad5fbea3151092bd6c08ddfc691cd96d2853e60f
Alan Ngo <ngoa2@uw.edu>
2020-08-15 23:44:11 -0700

    created nav bar

--- popup.js
+++ popup.js
@@ -39,5 +39,35 @@ chrome.storage.sync.get('color', function(data) {
     }
     // Relabel HTML
     elem.innerHTML = timeLeft + ' seconds remaining';
-  };
+  }
+
+    let testButton = document.getElementById('nav1');
+    let testButton2 = document.getElementById('nav2');
+    let testButton3 = document.getElementById('nav3');
+
+    var navBarChange = function(name, name2, name3) {
+      var content = document.getElementById(name);
+
+      // make others gone
+      var other1 = document.getElementById(name2);
+      var other2 = document.getElementById(name3);
+      other1.style.display = "none";
+      other2.style.display = "none";
+      content.style.display = "block";
+    }
+
+    testButton.onclick = function(){
+      console.log("test");
+      navBarChange("content1", "content2", "content3");
+    }
+    
+
+    testButton2.onclick = function(){
+      navBarChange("content2", "content1", "content3");
+    }
+
+    testButton3.onclick = function(){
+      navBarChange("content3", "content2", "content1");
+    }
+
     
